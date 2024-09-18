3b4227116b873bd629b9ac4065901c413f00a8e8
darkjozc <darkjos9@gmail.com>
2024-09-18 11:31:35 -0500

    agasdfgf
    asdfgasda

--- src/vehiculo/principal.java
+++ src/vehiculo/principal.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package vehiculo;
 
-/**
- *
- * @author Alumno-PB203
- */
+//autor:juan cana
 public class principal {
     public static void main(String[]arg){
         //iniciar = invocar = llamar a un objeto
@@ -20,3 +14,6 @@ public class principal {
     }
     
 }
+
+
+///aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
